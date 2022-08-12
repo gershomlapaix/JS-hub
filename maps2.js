90e2201e488ea9968b2130cc2b8f33c955d4c71e
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-12 23:07:27 +0200

    quiz

--- maps2.js
+++ maps2.js
@@ -1,4 +1,24 @@
-
+const weekDays = ["sun", "mon", "tue", "wed", "thur", "fri"];
+const restaurant = {
+  name: "classico Italiano",
+  location: "Via angelo",
+  categories: ["Italia", "pizzeria", "Vegetarian", "Organic"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    [weekDays[1]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekDays[2]]: {
+      open: 11,
+      close: 23,
+    },
+    [weekDays[3]]: {
+      open: 0,
+      close: 24,
+    },
+  },
+};
 
 // CONVENTION
 const question = new Map([
@@ -8,8 +28,22 @@ const question = new Map([
   [3, "JavaScript"],
   ["Correct", 3],
   [true, "Correct"],
-  [false, 'Try again']
+  [false, "Try again"],
 ]);
 
-
 console.log(question);
+
+// converting objects to map
+console.log(Object.entries(restaurant["openingHours"]));
+const hoursMap = new Map(Object.entries(restaurant["openingHours"]));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("Question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = Number(prompt('Your answer'))
+console.log(answer);
+console.log(question.get(question.get('Correct') === answer));
