949032a2d8a464d45b094bcbd7a26addb909d3e5
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-12 22:53:25 +0200

    maps convention

---
+++ maps2.js
@@ -0,0 +1,15 @@
+
+
+// CONVENTION
+const question = new Map([
+  ["Question", "What is the best programming language"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["Correct", 3],
+  [true, "Correct"],
+  [false, 'Try again']
+]);
+
+
+console.log(question);
