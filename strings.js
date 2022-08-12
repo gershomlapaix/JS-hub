547ff5b37ad3b7b02bd91e928cfa8af05f9e56f2
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-12 23:35:40 +0200

    strings, part1

---
+++ strings.js
@@ -0,0 +1,21 @@
+
+// PART 1
+const airline = "TAP Air Portugal";
+
+// basic
+console.log("B9B9B".length);
+console.log("B9B9B"[0]);
+console.log(airline.lastIndexOf("P"));
+console.log(airline.indexOf("Portugal"));
+
+// slice method:: to get new string
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+// get the first word of the sentence
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1)); // ge tthe last word
+
+
+// counting start from the end
+console.log(airline.slice(-2));
