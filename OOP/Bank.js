46af8e8ba8f54da0224d6c44e4053bc6bef92c68
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-13 23:42:58 +0200

    private fields and methods:: JS proposal touched

--- OOP/Bank.js
+++ OOP/Bank.js
@@ -1,28 +1,41 @@
 class Account {
-    constructor(owner, currency, pin){
-        this.owner = owner
-        this.currency = currency
-        this.pin = pin
-        this.movements = []
-        this.locale = navigator.language
+  // private fields
+  #movements = [];
+  #pin;
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    this.locale = navigator.language;
 
-        console.log(`Thanks for opening an account ${owner}`);
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  // define some methods
+
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withDraw(val) {
+    this.deposit(-val);
+  }
+
+  #approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
     }
-
-    // define some methods
-
-    deposit(val){
-        this.movements.push(val)
-    }
-
-    withDraw(val){
-        this.deposit(-val)
-    }
+  }
 }
 
-const acc1 = new Account('La paix', 'EUR',1111)
+const acc1 = new Account("La paix", "EUR", 1111);
 
-acc1.deposit(250)
-acc1.withDraw(140)
+acc1.deposit(250);
+acc1.withDraw(140);
 
 console.log(acc1);
